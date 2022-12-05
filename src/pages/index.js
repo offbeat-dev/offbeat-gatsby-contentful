06c87c06d6728cf04cd550b75ebc5f8f665fbc24
offbeat-dev <offbeat.it.all@gmail.com>
2022-12-05 13:00:38 -0600

    :sparkles: -- working with react-google-recaptcha / react-google-recaptcha-v3 has an issue with infinite loop

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,18 +4,35 @@ import Layout from "../components/layout"
 // import * as sections from "../components/sections"
 // import Fallback from "../components/fallback"
 import SEOHead from "../components/head"
-import {
-  GoogleReCaptchaProvider,
-  GoogleReCaptcha,
-} from "react-google-recaptcha-v3"
+// import {
+//   GoogleReCaptchaProvider,
+//   GoogleReCaptcha,
+// } from "react-google-recaptcha-v3"
+import GoogleRecaptcha from "react-google-recaptcha"
 
 export default function Homepage(props) {
   // const { homepage } = props.data
-  const [token, setToken] = React.useState()
+  const recaptchaRef = React.useRef(null)
+
+  const submitForm = async () => {
+    if (recaptchaRef.current === null) {
+      throw new Error(
+        "reCAPTCHA is enabled but reference not found, make sure you render the reCAPTCHA-component somewhere."
+      )
+    }
+    const isRecaptchaInvisible = recaptchaRef.current.props.size === "invisible"
+    const recaptchaValue = isRecaptchaInvisible
+      ? await recaptchaRef.current.executeAsync()
+      : recaptchaRef.current.getValue()
+
+    if (recaptchaValue) {
+      console.log("sending", recaptchaValue)
+    }
+  }
 
   const handleOnSubmit = (e) => {
     e.preventDefault()
-    console.log("Submitted", e, token)
+    submitForm()
   }
 
   // React.useEffect(() => {
@@ -24,21 +41,19 @@ export default function Homepage(props) {
 
   return (
     <Layout>
-      <GoogleReCaptchaProvider
-        reCaptchaKey={process.env.GATSBY_RECAPTCHA_V3_SITE_KEY}
-      >
-        <form onSubmit={handleOnSubmit}>
-          <input type="email" name="email" placeholder="Your Email" />
-          <input type="text" name="name" placeholder="Your Name" />
-          <input type="text" name="message" placeholder="Your Message" />
-          <button type="submit">Send</button>
-          <GoogleReCaptcha
-            onVerify={(token) => {
-              setToken(token)
-            }}
+      <form onSubmit={handleOnSubmit}>
+        <input type="email" name="email" placeholder="Your Email" />
+        <input type="text" name="name" placeholder="Your Name" />
+        <input type="text" name="message" placeholder="Your Message" />
+        <button type="submit">Send</button>
+        {process.env.GATSBY_RECAPTCHA_V3_SITE_KEY && (
+          <GoogleRecaptcha
+            sitekey={process.env.GATSBY_RECAPTCHA_V3_SITE_KEY}
+            ref={recaptchaRef}
+            size="invisible"
           />
-        </form>
-      </GoogleReCaptchaProvider>
+        )}
+      </form>
       {/* {homepage.blocks.map((block) => {
         const { id, blocktype, ...componentProps } = block
         const Component = sections[blocktype] || Fallback
