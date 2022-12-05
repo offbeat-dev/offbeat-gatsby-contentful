9167d9c15a25569efdffe04a11d69fb143f71411
offbeat-dev <offbeat.it.all@gmail.com>
2022-12-05 12:20:05 -0600

    :construction: -- testing google recaptcha

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,49 @@
 import * as React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
-import * as sections from "../components/sections"
-import Fallback from "../components/fallback"
+// import * as sections from "../components/sections"
+// import Fallback from "../components/fallback"
 import SEOHead from "../components/head"
+import {
+  GoogleReCaptchaProvider,
+  GoogleReCaptcha,
+} from "react-google-recaptcha-v3"
 
 export default function Homepage(props) {
-  const { homepage } = props.data
+  // const { homepage } = props.data
+  const [token, setToken] = React.useState()
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault()
+    console.log("Submitted", e, token)
+  }
+
+  // React.useEffect(() => {
+  //   console.log("Token", token)
+  // }, [token])
 
   return (
     <Layout>
-      {homepage.blocks.map((block) => {
+      <GoogleReCaptchaProvider
+        reCaptchaKey={process.env.GATSBY_RECAPTCHA_V3_SITE_KEY}
+      >
+        <form onSubmit={handleOnSubmit}>
+          <input type="email" name="email" placeholder="Your Email" />
+          <input type="text" name="name" placeholder="Your Name" />
+          <input type="text" name="message" placeholder="Your Message" />
+          <button type="submit">Send</button>
+          <GoogleReCaptcha
+            onVerify={(token) => {
+              setToken(token)
+            }}
+          />
+        </form>
+      </GoogleReCaptchaProvider>
+      {/* {homepage.blocks.map((block) => {
         const { id, blocktype, ...componentProps } = block
         const Component = sections[blocktype] || Fallback
         return <Component key={id} {...componentProps} />
-      })}
+      })} */}
     </Layout>
   )
 }
